50572cfffe72a37198cf09ca1fd97e9aedaa6b5c
Jakub Wadas <vadistic@gmail.com>
2020-10-27 10:56:22 +0000

    7

---
+++ leetcode/7.reverse-integer.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const MIN_INT =  -2147483648
+    const MAX_INT = 2147483647
+
+    if(x === 0) return 0
+
+    const sign = x < 0 ? -1 : 1
+
+    const unsignedStr = Math.abs(x).toString().split('').reverse().join('')
+
+    const result = Number(unsignedStr) * sign
+
+    if(result < MIN_INT || result > MAX_INT) return 0
+
+    return result
+};
+
+export default reverse
