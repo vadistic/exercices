f6968a977f79e6f9035778b1f085bc9190954d4c
Jakub Wadas <vadistic@gmail.com>
2020-10-27 10:56:11 +0000

    8

--- leetcode/8.string-to-atoi.js
+++ leetcode/8.string-to-atoi.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var myAtoi = function (s) {
-  let str = s.trimStart()
+  const str = s.trimStart()
 
+  /**
+   * @type {number}
+   */
+  let sign
   let resultSrt = ''
-  let sign = str[0] === '-' ? -1 : 1
 
   const numRe = /[0-9]/
 
   for (const char of str) {
-    if (resultSrt === '' && (char === '+' || char === '-')) {
+    if (!sign && !resultSrt && (char === '+' || char === '-')) {
+      sign = char === '-' ? -1 : 1
       continue
     }
 
@@ -26,12 +30,12 @@ var myAtoi = function (s) {
   const MIN_INT = -1 * Math.pow(2, 31)
   const MAX_INT = Math.pow(2, 31) - 1
 
-  const result = sign * Number(resultSrt)
+  const result = sign === -1 ? -1 * Number(resultSrt) : Number(resultSrt)
 
   if (result <= MIN_INT) return MIN_INT
   if (result >= MAX_INT) return MAX_INT
 
-  return sign * result
+  return result
 }
 
 export default myAtoi
