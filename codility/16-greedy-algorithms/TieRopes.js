8e98851915af60b459721c5a42d4737d1e3a035b
Jakub Wadas <vadistic@gmail.com>
2020-10-29 23:48:27 +0000

    tie ropes

---
+++ codility/16-greedy-algorithms/TieRopes.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param {number} K
+ * @param {number[]} A
+ */
+function solution(K, A) {
+  let count = 0
+
+  let length = 0
+
+  for (const rope of A) {
+    length += rope
+
+    if (length >= K) {
+      count += 1
+      length = 0
+    }
+  }
+
+  return count
+}
